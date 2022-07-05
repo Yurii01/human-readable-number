b11a8104527cd817cf69e36933111b9039673f1f
Yurii01 <halasyurii@gmail.com>
2022-07-05 15:17:48 +0300

    init: add task human-readable-number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    const numberToWords = require("number-to-words");
+    return numberToWords.toWords(number).replace(/-/g, " ");
+};
